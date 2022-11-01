e0f5596047d701e621994149f1ae63dff0abbb46
Valeria Balatskaya <valeriabalacka@gmail.com>
2022-11-01 14:51:30 +0100

    Integrated photo API

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
         <h1>Dictionary</h1>
        </header>
        <main>
-        <Dictionary defaultKeyword="sunset" />
+        <Dictionary defaultKeyword="night" />
        </main>
        <footer className='App-footer'>
         Coded by Valeria Balatska
